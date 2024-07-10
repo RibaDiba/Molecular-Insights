c34873f3f49d72f5dc4ad9039a2b97d46942adb0
RibaDiba <abrmod74@gmail.com>
2024-07-10 19:16:59 -0400

    added packages

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,24 @@
+<script>
+
+     import { ShaderGradientCanvas, ShaderGradient } from 'shadergradient'
+
+</script>
+
+<main>
+     <nav class="fixed top-0 left-0 w-full bg-white bg-opacity-50 backdrop-blur-md shadow-md text-2xl">
+          <div class="container mx-auto flex justify-between items-center p-4 text-blue-900">
+               <div class="logo">
+                    <a href="/" class="">Molecular Insights</a>
+               </div>
+               <ul class="flex space-x-10">
+                    <li><a href="#home" class="hover:text-blue-700">About Us</a></li>
+                    <li><a href="/about" class="hover:text-blue-700">Blog</a></li>
+                    <li><a href="/instalin">
+                         
+                    </a></li>
+               </ul>
+          </div>
+     </nav>
+
+
+</main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+     import { Button } from '$lib/components/ui/button'
+     import Navbar from '$lib/components/Navbar.svelte'
+</script>
+
+<main>
+     <Navbar />
+</main>
