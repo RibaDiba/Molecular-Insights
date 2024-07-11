ba5e65b48d22f283a9382995feaa42dfe9af5edd
RibaDiba <abrmod74@gmail.com>
2024-07-11 09:02:23 -0400

    added content and auto scroll

---
+++ src/lib/components/MissionStatement.svelte
@@ -0,0 +1,43 @@
+<script>
+
+     import * as Carousel from '$lib/components/ui/carousel';
+     import * as Card from '$lib/components/ui/card'
+
+     const mission = 'Our mission is to inspire curiosity and foster a deeper understanding of biomedical science within our community. Through engaging and informative blog articles and social media posts, we aim to make complex scientific concepts accessible and interesting for students, educators, and anyone passionate about the life sciences. By sharing our passion for biomedical discoveries, we hope to encourage future generations to pursue careers in STEM and contribute to the advancement of healthcare and medicine. Join us on our journey to explore the fascinating world of biomedical science and make a difference in our community! Follow our blog and Instagram for the latest articles, insights, and updates. Together, we can ignite a passion for science and inspire the next generation of biomedical innovators.'
+
+
+</script>
+
+<main>
+
+     <div class="flex justify-around">
+          <div class="w-3/5">
+               <h1>Our Mission: </h1>
+               <p>{mission}</p>
+          </div>
+
+          <div class="justify-center items-center">
+               <Carousel.Root class="w-full max-w-xs">
+                    <Carousel.Content>
+                      {#each Array(5) as _, i (i)}
+                        <Carousel.Item>
+                          <div class="p-1">
+                            <Card.Root>
+                              <Card.Content
+                                class="flex aspect-square items-center justify-center p-6"
+                              >
+                                <span class="text-4xl font-semibold">{i + 1}</span>
+                              </Card.Content>
+                            </Card.Root>
+                          </div>
+                        </Carousel.Item>
+                      {/each}
+                    </Carousel.Content>
+                    <Carousel.Previous />
+                    <Carousel.Next />
+               </Carousel.Root>
+          </div>
+          
+     </div>
+
+</main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,24 +2,34 @@
      import { Button } from '$lib/components/ui/button'
      import Navbar from '$lib/components/Navbar.svelte'
      import { TextGenerateEffect } from '$lib/components/ui/TextGenerateEffect'
+     import MissionStatement from '@/components/MissionStatement.svelte'
 
      const words = 'Molecular Insights'
+
+     function handleScroll() {
+          const missionElement = document.getElementById('mission-statement')
+          missionElement.scrollIntoView({ behavior: 'smooth' })
+     }
+
 </script>
 
-<main class="relative min-h-screen">
-     
+<main class="relative min-h-screen flex flex-col">
+    
 
-     <div class="bg-blue-200 flex flex-col justify-center items-center w-screen h-screen absolute top-0 left-0">
-          <img src="src/lib/images/shape-background.jpeg" alt="" class="w-full h-full object-cover">
-          <div class="absolute flex flex-col items-center justify-center w-full h-full">
+     <div class="bg-blue-200 flex flex-col justify-center items-center w-full h-screen relative">
+          <img src="src/lib/images/shape-background.jpeg" alt="" class="w-full h-full object-cover absolute top-0 left-0">
+          <div class="relative flex flex-col items-center justify-center w-full h-full">
                <span class="text-white text-9xl px-4 py-2 rounded">
                     <TextGenerateEffect {words}/>
                </span>
 
-               <Button size='lg' variant='default' class="bg-blue-950 hover:bg-blue-900 mt-10 text-xl p-8 font-mono">Learn More</Button>
+               <Button size='lg' variant='default' class="bg-blue-950 hover:bg-blue-900 mt-10 text-xl p-8 font-mono" on:click={handleScroll}>Learn More</Button>
           </div>
-          <Navbar />
      </div>
 
-     
+     <Navbar />
+     <div id="mission-statement">
+          <MissionStatement/>
+     </div>
+
 </main>
