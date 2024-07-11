b4dd93e036f446ac985b205f2ce08deaf1065abf
RibaDiba <abrmod74@gmail.com>
2024-07-10 20:18:48 -0400

    created a basic front page

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,24 +1,26 @@
 <script>
 
-     import { ShaderGradientCanvas, ShaderGradient } from 'shadergradient'
+     import { TextGenerateEffect } from "$lib/components/ui/TextGenerateEffect";
+     import Button from "./ui/button/button.svelte";
 
+     const words = 'Molecular Insights'
 </script>
 
 <main>
-     <nav class="fixed top-0 left-0 w-full bg-white bg-opacity-50 backdrop-blur-md shadow-md text-2xl">
-          <div class="container mx-auto flex justify-between items-center p-4 text-blue-900">
-               <div class="logo">
-                    <a href="/" class="">Molecular Insights</a>
+     <div class="bg-blue-200">
+          <nav class="fixed top-0 left-0 w-full bg-white bg-opacity-30 backdrop-blur-md shadow-md text-3xl font-serif">
+               <div class="container mx-auto flex justify-between items-center p-4 text-blue-900">
+                    <div class="logo">
+                         <a href="/" class="font-bold">Molecular Insights</a>
+                    </div>
+                    <ul class="flex space-x-10">
+                         <li><a href="#home" class="hover:text-blue-700">About Us</a></li>
+                         <li><a href="/about" class="hover:text-blue-700">Blog</a></li>
+                         <li><a href="/instalin">
+                              
+                         </a></li>
+                    </ul>
                </div>
-               <ul class="flex space-x-10">
-                    <li><a href="#home" class="hover:text-blue-700">About Us</a></li>
-                    <li><a href="/about" class="hover:text-blue-700">Blog</a></li>
-                    <li><a href="/instalin">
-                         
-                    </a></li>
-               </ul>
-          </div>
-     </nav>
-
-
+          </nav>
+     </div>
 </main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,30 @@
 <script>
      import { Button } from '$lib/components/ui/button'
      import Navbar from '$lib/components/Navbar.svelte'
+     import { TextGenerateEffect } from '$lib/components/ui/TextGenerateEffect'
+
+     const words = 'Molecular Insights'
 </script>
 
-<main>
+<main class="">
      <Navbar />
+
+     <div class="bg-blue-200 flex justify-center first-comp">
+          <div class="absolute flex flex-col items-center justify-center">
+               <span class="text-white text-9xl px-4 py-2 rounded">
+                    <TextGenerateEffect {words}/>
+               </span>
+
+               <Button size='lg' variant='default' class="bg-blue-950 hover:bg-blue-900 mt-10 text-xl p-8 font-mono">Learn More</Button>
+          </div>
+     </div>
+
+
+
 </main>
+
+<style>
+     .first-comp {
+          margin: 35dvh;
+     }
+</style>
