ae5c633fc35973760f76d635d1697f1ce031d7c9
RibaDiba <abrmod74@gmail.com>
2024-07-31 11:18:44 -0400

    added routes

--- api/routes/article.js
+++ api/routes/article.js
@@ -1,6 +1,60 @@
 const express = require('express');
 const router = express.Router();
-const mongoose = require('mongoose');
-const multer = require('multer');
-const upload = multer({dest: '/uploads/'})
 
+
+// has to be '/' because all /article requests are already coming here
+router.get('/', (req, res, next) => {
+     res.status(200).json({
+          message: "Get Request to /article"
+     })
+})
+
+router.post('/', (req, res, next) => {
+
+     const article = {
+          "title": req.body.title,
+          "content": req.body.content,
+          "image": req.body.image,
+          "author": req.body.author,
+          "datePublished": req.body.datePublished
+     }
+
+     res.status(200).json({
+          message: "worked",
+          createdArticle: {
+               article: article
+          }
+     })
+})
+
+router.get('/:articleTitle', (req, res, next) => {
+     const title = req.params.articleTitle
+     if (title == "mole") {
+          res.status(200).json({
+               message: 'mole title',
+               title: title
+          })
+     } else {
+          res.status(200).json({
+               message: "passed a title"
+          })
+     }
+})
+
+router.patch('/:articleTitle', (req, res, next) => {
+     const title = req.params.articleTitle
+     if (title == "mole") {
+          res.status(200).json({
+               message: 'mole title',
+               title: title
+          })
+     } else {
+          res.status(200).json({
+               message: "passed a title"
+          })
+     }
+})
+
+
+
+module.exports = router
--- app.js
+++ app.js
@@ -3,40 +3,46 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyPaarser = require('body-parser');
-const userRoutes = require('./api/routes/users');
-const imageRoutes = require('./api/routes/images');
 const mongoose = require('mongoose');
 
 // apply imports 
 app.use(morgan('dev'));
-app.use('/uploads',express.static('uploads'))
-app.use(bodyPaarser.urlencoded({extended: false}));
+app.use(bodyPaarser.urlencoded({ extended: false }));
 app.use(bodyPaarser.json());
 
+// routes 
+const articleRoutes = require('./api/routes/article')
+
+// enable routes
+app.use('/article', articleRoutes)
+
 // enable cors
-const cors=require("cors");
-const corsOptions ={
-   origin:'*', 
-   credentials:true,           
-   optionSuccessStatus:200,
+const cors = require("cors");
+const corsOptions = {
+     origin: '*',
+     credentials: true,
+     optionSuccessStatus: 200,
 }
 
-app.use(cors(corsOptions)) 
+app.use(cors(corsOptions))
 
-app.use((req,res,next) => {
+app.use((req, res, next) => {
      const error = new Error("Not Found");
      error.status = 404;
      next(error);
- })
- 
- app.use((error, req, res, next) => {
+})
+
+app.use((error, req, res, next) => {
      res.status(error.status || 500);
      res.json({
-         error: {
-             message: error.message
-         }
+          error: {
+               message: error.message
+          }
      })
- })
+})
+
+// handle all /article requests to the article routes folder
+
 
 module.exports = app
- 
+
