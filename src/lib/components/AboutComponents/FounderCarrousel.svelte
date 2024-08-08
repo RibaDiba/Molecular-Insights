ee04820add6554c3232de1a4319478fc2bfb5808
RibaDiba <abrmod74@gmail.com>
2024-08-08 10:22:28 -0400

    saving

---
+++ src/lib/components/AboutComponents/FounderCarrousel.svelte
@@ -0,0 +1,148 @@
+<script>
+    import { Splide, SplideSlide, SplideTrack } from '@splidejs/svelte-splide';
+    import arrow from '../../images/arrows/arrow.png';
+  
+    export let coFounders;
+    export let insta;
+  </script>
+  
+  <main class="everything-container">
+    <Splide
+      aria-label="Meet Our Co-Founders"
+      options={{
+        padding: '5rem',
+        type: 'loop',
+        width: '90%'
+      }}
+      hasTrack={false}
+    >
+      <SplideTrack>
+          {#each coFounders as founder}
+          <SplideSlide>
+              <div class="founder-container flex p-10 px-20">
+                  <div class="founder-image">
+                      <img src={founder.image} alt="" />
+                  </div>
+                  <div class="founder-details flex justify-center flex-col poppins text-lg px-10">
+                      <div class="label mt-3 text-blue-950">{founder.name}</div>
+                      <div class="title text-gray-400">{founder.title}</div>
+                      <div class="description text-gray-600 mt-2">{founder.description}</div>
+                      <div class="social-icons mt-3">
+                          <a href={founder.instaLink}><img src={insta} alt="" /></a>
+                      </div>
+                  </div>
+              </div>
+          </SplideSlide>
+          {/each}
+      </SplideTrack>
+  
+      <div class="splide__arrows arrow-container">
+          <button class="splide__arrow splide__arrow--prev btn">
+              <img src={arrow} alt="" class="arrow-image">
+          </button>
+          <button class="splide__arrow splide__arrow--next btn">
+              <img src={arrow} alt="" class="arrow-image flipped">
+          </button>
+      </div>
+    </Splide>
+  </main>
+  
+  <style>
+      .social-icons img {
+          width: 30px;
+          height: 30px;
+      }
+  
+      .founder-image img {
+          width: 300px;
+          height: 300px;
+          object-fit: cover;
+      }
+  
+      .btn {
+          border-radius: 3px;
+          border: 4px solid #172554;
+          padding: 20px;
+          background-color: #172554;
+          transition: 0.1s;
+          width: 100px;
+          height: 100px;
+      }
+  
+      .btn:hover {
+          background-color: transparent;
+      }
+  
+      .arrow-container {
+          display: flex;
+          justify-content: space-between;
+          position: absolute;
+          top: 50%;
+          width: 100%;
+          transform: translateY(-50%);
+          pointer-events: none;
+      }
+  
+      .splide__arrow {
+          pointer-events: all;
+          width: 100px;
+          height: 100px;
+      }
+  
+      .splide__arrow--prev {
+          left: 0;
+      }
+  
+      .splide__arrow--next {
+          right: 0;
+      }
+  
+      .everything-container {
+          justify-content: center;
+          align-items: center;
+          display: flex;
+          overflow: hidden;
+          width: 100%;
+          padding: 0;
+          box-sizing: border-box;
+      }
+  
+      .arrow-image {
+          width: 100%;
+          height: 100%;
+      }
+  
+      .arrow-image img {
+          width: 100%;
+          height: 100%;
+          object-fit: contain;
+      }
+  
+      .flipped {
+          transform: rotate(180deg);
+      }
+  
+      .poppins {
+          font-family: 'Poppins';
+          font-size: 24px;
+      }
+  
+      .description {
+          font-size: 14px;
+      }
+  
+      .founder-container {
+          display: flex;
+          flex-direction: row;
+          align-items: center;
+          gap: 20px;
+          width: 100%;
+          margin: 0;
+      }
+  
+      .founder-details {
+          flex: 1;
+          margin-left: 50px;
+      }
+  </style>
+  
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -15,13 +15,14 @@
 
 	// or only core styles
 	import "@splidejs/svelte-splide/css/core";
+  import FounderCarrousel from "@/components/AboutComponents/FounderCarrousel.svelte";
 
   const coFounders = [
     {
       name: "Ishwari",
       title: "Co-Founder",
       instaLink: "",
-      description: "",
+      description: "I am a rising senior at John P Stevens High School. I am interested in majoring in Biomedical Engineering and pursuing a medical degree in the future. I wanted to create this website and blog to educate myself on science topics and explore my passion for medical learnings. I wish to share my knowledge and inspire young middle and elementary schoolers in the magnificent world of science.",
       linkedIn: "",
       image: ishwari
     },
@@ -29,7 +30,7 @@
       name: "Anumeha",
       title: "Co-Founder",
       instaLink: "",
-      description: "",
+      description: "I am a rising senior at John P Stevens High School. I am interested in pursuing pharmacology and leaning into medicinal research. My hobbies include Bharatnatyam and listening to music. Growing up, I've always had my cultural classes outside of school but during school I was always enamored by science. As I entered high school, I found my love for chemistry and have since pursued as many opportunities in medicinal chemistry as I can.  I wanted to create this website to keep people in our community updated about new feats in science while allow more inspire more students to find their love for science.",
       linkedIn: "",
       image: anumeha
     }
@@ -69,13 +70,18 @@
 	];
 </script>
 
-<main>
+<main class="">
 	<Navbar />
 
-	<div class="mt-40 flex flex-col text-center">
+	<div class="mt-20 flex flex-col text-center">
 		<h1 class="meet-header justify-center">Meet Our Team</h1>
 		<div class="divider mx-64 mt-10 bg-blue-950"></div>
 	</div>
+
+	<div  class=" mt-20 flex items-center justify-center items-center bg-blue-100 py-10">
+		<FounderCarrousel {coFounders} {insta} />
+	  </div>
+	
 
 	<div class="mx-64 mt-20 flex items-center justify-center bg-blue-100 p-20">
 		<Splide
@@ -103,30 +109,6 @@
 		</Splide>
 	</div>
 
-  <div  class="mx-64 mt-20 flex items-center justify-center bg-blue-100 p-20">
-
-    <Splide aria-label="Meet Our Co-Founders" 
-      options={{
-        // padding: '5rem'
-      }}
-    >
-      {#each coFounders as founder}
-        <SplideSlide>
-          <div class="slide-container flex justify-start">
-            <div class="image-container founder-image-container">
-              <img src={founder.image} alt="">
-            </div>
-            <div class="label">{founder.name}</div>
-            <div class="title">{founder.title}</div>
-            <div class="social-icons justify-center mb-10 flex">
-              <a href={founder.instaLink}><img src={insta} alt=""></a>
-            </div>
-          </div>
-        </SplideSlide>
-      {/each} 
-    </Splide>
-
-  </div>
 
 	<div class="mt-20">
 		<FooterComponent />
@@ -192,6 +174,10 @@
   .founder-image-container {
     margin: 10px;
     padding: 0rem;
+  }
+
+  .poppins {
+	font-family: 'poppins';
   }
 
 </style>
